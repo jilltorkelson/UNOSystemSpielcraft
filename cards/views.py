1a6c12911b7640d405c816669f60e7e3a96936e0
jilltorkelson <JillT8585@gmail.com>
2024-03-14 20:41:11 -0500

    Run homepage functionally
    This is reverted to test and run templates functionally. Login & signup functions will be reimplemented using built in Django functions

--- cards/views.py
+++ cards/views.py
@@ -1,15 +1,16 @@
-from .models import Card,UserCard, Decks, DeckCards, TradeRequest, TradeResponse, TradeStatus, OfferedCard, RequestedCard
+from .models import Card, UserCard, Decks, DeckCards, TradeRequest, TradeResponse, TradeStatus, OfferedCard, \
+    RequestedCard
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
+    """View function for the home page"""
     num_cards = Card.objects.all().count()
-    num_decks = Decks.objects.count()
+    num_decks = Decks.objects.all().count()
 
     context = {
         'num_cards': num_cards,
         'num_decks': num_decks,
     }
-
     return render(request, 'index.html', context=context)
-
