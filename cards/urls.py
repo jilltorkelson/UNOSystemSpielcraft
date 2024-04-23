ebf586b5f834b05d1f58c3903669e3bef4fb8dfd
jilltorkelson <JillT8585@gmail.com>
2024-04-23 18:10:16 -0500

    Update urls.py

--- cards/urls.py
+++ cards/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('card_repository/', views.CardRepositoryView.as_view(), name='card_repository'),
-    path('my_trade_requests/', views.MyTradeRequestsView.as_view(), name='my_trade_requests'),
+    path('trade_request_list/', views.MyTradeRequestsView.as_view(), name='trade_request_list'),
     path('my_cards/', views.MyCardsListView.as_view(), name='my_cards'),
     path('my_decks/', views.MyDecksListView.as_view(), name='my_decks'),
     path('card_detail/<int:pk>', views.CardDetailView.as_view(), name='card_detail'),
