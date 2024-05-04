259b24d93d115337d90c2a880dc30fc7a27f5cef
David Lape <lapedavi@isu.edu>
2024-05-04 10:00:56 -0500

    Removed duplicate properties, added table to trade request list, removed uneeded request

--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Card, UserCard, Decks, TradeRequest, OfferedCard, RequestedCard, DeckCards
 from .forms import UserCardForm
+
 
 def index(request):
     """View function for the home page"""
@@ -19,15 +22,18 @@ def index(request):
     }
     return render(request, 'index.html', context=context)
 
+
 class CardRepositoryView(LoginRequiredMixin, ListView):
     """View for the card repository page"""
     model = Card
     template_name = 'cards/card_repository.html'
 
+
 class CardDetailView(LoginRequiredMixin, ListView):
     """View for the card detail page"""
     model = Card
     template_name = 'cards/card_detail.html'
+
 
 class MyCardsListView(LoginRequiredMixin, ListView):
     """View to list cards owned by logged-in user"""
@@ -38,6 +44,7 @@ class MyCardsListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return UserCard.objects.filter(player=self.request.user)
 
+
 class MyDecksListView(LoginRequiredMixin, ListView):
     """View for the my decks page"""
     model = Decks
@@ -47,27 +54,14 @@ class MyDecksListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return Decks.objects.filter(player=self.request.user)
 
+
 class TradeRequestListView(LoginRequiredMixin, View):
     """View to handle trade requests"""
     def get(self, request, *args, **kwargs):
         """Handle GET request to retrieve and display trade requests"""
-        trade_requests = TradeRequest.objects.all()
-        form = UserCardForm()
-        return render(request, 'cards/trade_request_list.html', {'trade_requests': trade_requests, 'form': form})
+        trade_requests = TradeRequest.objects.filter(status='p')
+        return render(request, 'cards/trade_request_list.html', {'trade_requests': trade_requests})
 
-    def post(self, request, *args, **kwargs):
-        """Handle POST request to process form submission for creating a new trade request"""
-        form = UserCardForm(request.POST)
-        if form.is_valid():
-            new_trade_request = TradeRequest.objects.create(playerRequesting=request.user)
-            for card in form.cleaned_data['offered_cards']:
-                OfferedCard.objects.create(trade_request_id=new_trade_request, user_card_id=card, offered_card_quantity=1)
-            for card in form.cleaned_data['requested_cards']:
-                RequestedCard.objects.create(trade_request_id=new_trade_request, card_id=card)
-            return redirect('my_trade_requests')
-
-        trade_requests = TradeRequest.objects.all()
-        return render(request, 'cards/trade_request_list.html', {'trade_requests': trade_requests, 'form': form})
 
 def trade_request_create_view(request):
     """View function to handle creation of a new trade request"""
@@ -76,7 +70,8 @@ def trade_request_create_view(request):
         if form.is_valid():
             offered_cards = form.cleaned_data['offered_cards']
             requested_cards = form.cleaned_data['requested_cards']
-            new_trade_request = TradeRequest.objects.create(playerRequesting=request.user)
+            new_trade_request = TradeRequest.objects.create(
+                playerRequesting=request.user, trade_request_date=datetime.now())
             for card in offered_cards:
                 OfferedCard.objects.create(trade_request_id=new_trade_request, user_card_id=card, offered_card_quantity=1)
             for card in requested_cards:
